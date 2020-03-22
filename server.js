00a4536ef34223f5f039e946033db1035eb068e1
frederico-neto <frederico.neto@webtales.fr>
2020-03-22 15:34:46 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 
 const app = express();
-
+const port = 3000;
 
 // Set public folder as root
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // Provide access to node_modules folder from the client-side
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Redirect all traffic to index.html
-//app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`)
-  });
-
-app.get('/js/app.js', (req, res) => {
-res.sendFile(`${__dirname}/public/js/app.js`)
+app.listen(port, () => {
+  console.info('listening on %d', port);
 });
-
-  https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app).listen(3001, () => {
-    console.log('Listening...')
-  })
 
 
 
